dcecf434020ab1fd1451c2a6cf36d1502b52c55f
NikPurvis <nikpurvis@gmail.com>
2022-04-22 16:34:10 -0600

    Added create, update, and delete views

--- bookmark/main_app/urls.py
+++ bookmark/main_app/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path("bookshelf/", views.bookshelf, name="bookshelf"),
     path("books/", views.books_index, name="books_index"),
     path("books/<int:book_id>/", views.books_show, name="books_show"),
+    path("books/create/", views.BookCreate.as_view(), name="book_create"),
+    path("book/<int:pk>/update/", views.BookUpdate.as_view(), name="book_update"),
+    path("book/<int:pk>/delete/", views.BookDelete.as_view(), name="book_delete"),
     path("bookclub/", views.bookclub, name="bookclub"),
     path("search/", views.search, name="search")
 ]
--- bookmark/main_app/views.py
+++ bookmark/main_app/views.py
@@ -3,10 +3,15 @@
 # Import dependencies
 from django import http
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Import models
 from .models import Book
 
+
+# Views (like routes) created below.
+# They're functions that are called by main_app's urls.py file.
+# Those urls are registered in the project's (bookmark) urls.py.
 
 # Index view
 def index(request):
@@ -26,6 +31,7 @@ def books_index(request):
     return render(request, "books/index.html", { "books": books })
 
 # Books detail view
+# Get an ID from the route parameter, defined in the url
 def books_show(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, "books/show.html", { "book": book })
@@ -41,3 +47,35 @@ def search(request):
 # Book community view
 # def book_comm(request):
 #     return HttpResponse("<h1>Book community page!</h1>")
+
+
+# Defining a class to create books using Django's built-in methods
+class BookCreate(CreateView):
+    # Bases the form on the books model
+    model = Book
+    # Include all the fields on the form
+    fields = "__all__"
+    
+    # It's not stricly necessary to call this method, but if we want to redirect to the details view page, we need to use this to get its primary key
+    def form_valid(self, form):
+        # Create an object from the form
+        self.object = form.save(commit=False)
+        # Save the object to the database
+        self.object.save()
+        # Redirect to its new detail page
+        return HttpResponseRedirect("/cats" + str(self.object.pk))
+
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = "__all__"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect("/cats" + str(self.object.pk))
+
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = "/books"
